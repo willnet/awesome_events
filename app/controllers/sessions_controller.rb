36e804d5ff79c743380bd94c81edc3e64ec176c7
willnet <netwillnet@gmail.com>
2013-10-30 00:47:55 +0900

    ログイン後にフラッシュメッセージを表示するようにした。ログアウト処理を途中まで書いた

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to root_path, notice: 'ログインしました'
+  end
+
+  def destroy
+    reset_sessions
+    redirect_to root_path, notice: 'ログアウトしました'
   end
 end
