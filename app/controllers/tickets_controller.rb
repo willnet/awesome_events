59fdc848460dbece6cf322d2eb04f9941e3d7bd8
willnet <netwillnet@gmail.com>
2013-11-11 23:33:49 +0900

    イベントキャンセルの際にあり得ない event_id を送信したとき、500になっていたのを404になるように修正した

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -19,7 +19,7 @@ class TicketsController < ApplicationController
   end
 
   def destroy
-    ticket = current_user.tickets.find_by(event_id: params[:event_id])
+    ticket = current_user.tickets.find_by!(event_id: params[:event_id])
     ticket.destroy!
     redirect_to event_path(params[:event_id]), notice: 'このイベントの参加をキャンセルしました'
   end
