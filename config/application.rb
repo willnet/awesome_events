82b56e912516f16e4a05a098e3b98204e1a90b45
willnet <netwillnet@gmail.com>
2013-12-18 00:49:50 +0900

    deprication warning を抑制

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module AwesomeEvents
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    I18n.enforce_available_locales = true
     config.i18n.default_locale = :ja
   end
 end
