a3d80da50d6b1e7aa5e7ca9642e82a23c1fdbcc6
willnet <netwillnet@gmail.com>
2014-01-12 18:03:17 +0900

    development だと twitterログインに失敗した時に 401 になるのを、他の environment と同じ挙動になるように修正した

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'CWUih1lMjsh4Itzflq5yKg', 'xz0HNek2VvvodgBcjxz9mcGewQAUB9GX9fToBRrg'
 end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
