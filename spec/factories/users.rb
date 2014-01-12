742b8e70510fd57fff9ea5757f7ac33094e5ef54
willnet <netwillnet@gmail.com>
2014-01-12 17:58:31 +0900

    typo

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user, aliases: [:owner] do
     provider 'twitter'
-    sequence(:uid) { |i| "uid{i}" }
+    sequence(:uid) { |i| "uid#{i}" }
     sequence(:nickname) { |i| "nickname#{i}" }
     sequence(:image_url) { |i| "http://example.com/image#{i}.jpg"}
   end
