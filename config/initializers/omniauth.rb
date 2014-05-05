bd94e892d2c84b70260800ecf9abf5eb99bc1e6f
willnet <netwillnet@gmail.com>
2014-05-05 19:15:24 +0900

    Rename consumer key and consumer secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 
-    Rails.application.secrets.twitter_consumer_key,
-    Rails.application.secrets.twitter_consumer_secret
+    Rails.application.secrets.twitter_api_key,
+    Rails.application.secrets.twitter_api_secret
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
