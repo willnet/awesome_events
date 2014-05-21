0eb1a1177aaf1a777a8b2dc56cce3fed2d4227e7
willnet <netwillnet@gmail.com>
2014-05-21 14:38:35 +0900

    error404 メソッドの場所と引数が正しくなくて、存在しないパスを指定した時に404が返っていなかったのを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,15 @@ class ApplicationController < ActionController::Base
   rescue_from Exception, with: :error500
   rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
 
+  def error404
+    render 'error404', status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    logger.error [e, *e.backtrace].join("\n")
+    render 'error500', status: 500, formats: [:html]
+  end
+
   private
 
   def current_user
@@ -21,14 +30,5 @@ class ApplicationController < ActionController::Base
   def authenticate
     return if logged_in?
     redirect_to root_path, alert: 'ログインしてください'
-  end
-
-  def error404(e)
-    render 'error404', status: 404, formats: [:html]
-  end
-
-  def error500(e)
-    logger.error [e, *e.backtrace].join("\n")
-    render 'error500', status: 500, formats: [:html]
   end
 end
